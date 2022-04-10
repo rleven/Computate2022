5efa72f7609377f2c07a029824b462eb441357e6
Richard Leven <richard.leven@udo.edu>
2022-04-10 16:05:26 +0200

    Tried some commands in plot.py

--- Blatt0/plots.py
+++ Blatt0/plots.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x2 = np.genfromtxt("build/teil1.txt", encoding="utf-16-le", unpack=True, usecols=0, delimiter=",")
-y2 = np.genfromtxt("build/teil1.txt", encoding="utf-16-le", unpack=True, usecols=1, delimiter=",")
+x2 = np.genfromtxt("build/teil1.txt", delimiter=',', unpack=True, usecols=0)
+y2 = np.genfromtxt("build/teil1.txt", unpack=True, usecols=1, delimiter=",")
 
-plt.plot(x2, y2, -k, label="Teil 1")
+plt.plot(x2, y2, '-k', label="Teil 1")
 plt.legend(loc="best")
 plt.show()
