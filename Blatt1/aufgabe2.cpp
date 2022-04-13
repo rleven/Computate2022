be12f4b5c6741f2877aac93bb320f80d2cd39556
Richard Leven <richard.leven@udo.edu>
2022-04-13 11:11:42 +0200

    aufgabe2 is now complete

--- Blatt1/aufgabe2.cpp
+++ Blatt1/aufgabe2.cpp
@@ -14,6 +14,7 @@ typedef double (*function) (double);
 
 /* This function iterates and saves according to the Trapezoidal rule */
 double trapez(function blank, double a, double b, int N){
+
     /* Calculation of the step size h and initial values of the integration */
     double h = (b -a)/N;
     double integration = blank(a) + blank(b);
@@ -34,8 +35,59 @@ double trapez(function blank, double a, double b, int N){
     return integration*(h/2);
 }
 
+/* This function iterates and saves according to the Midpoint rule */
+double midpoint(function blank, double a, double b, int N){
+
+    /* Calculation of the step size h and initial values of the integration */
+    double h=(b-a)/N;
+    double integration = 0;
+
+    /* Generating of savefile */
+    ofstream midpoint;
+    midpoint.open("data/midpoint.txt", ios_base::trunc);
+    midpoint << "#h   y";
+
+    /* Iteration of midpoint rule */
+    for (int i=1; i<=N; i++){
+        integration = integration + blank(a-h/2+i*h);
+        midpoint << "\n" << i*h << "    " << integration*h;
+    }
+
+    /* Returns final integration value after N iterations */
+    return integration*h;
+}
+
+double simpson(function blank, double a, double b, int N){
+
+    /* Calculation of the step size h and initial values of the integration */
+    double h = (b-a)/N;
+    double integration = (1.0/3.0)*blank(a) + (1.0/3.0)*blank(b);
+
+    /* Generating of savefile */
+    ofstream simpson;
+    simpson.open("data/simpson.txt", ios_base::trunc);
+    simpson << "#h   y";
+
+    /* Iteration of simpson rule */
+    for (int i=1; i<=N-1; i++){
+        /* Checks if step i is odd */
+        if (i % 2 != 0) {
+        integration = integration + (4.0/3.0)*blank(a+i*h);
+        }
+        /* Checks if step i is even */
+        if (i % 2 == 0){
+        integration = integration + (2.0/3.0)*blank(a+i*h);
+        }
+        simpson << "\n" << i*h << " " << integration*h;
+    }
+
+    /* Returns final integration number after N-1 iterations */
+    return integration*h;
+}
+
 int main(){
-    
-    cout << trapez(f, 10, 15, 100);
+    cout << trapez(f, 10, 15, 100) << endl;
+    cout << midpoint(f, 10, 15, 100) << endl;
+    cout << simpson(f, 10, 15, 100) << endl;
     return 0;
 }
