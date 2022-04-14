078ebfbb559d287c712289a2d38be499c790a673
Richard Leven <richard.leven@udo.edu>
2022-04-14 13:12:22 +0200

    Made it look nicer :)

--- Blatt1/aufgabe1.cpp
+++ Blatt1/aufgabe1.cpp
@@ -4,38 +4,33 @@
 #include <fstream>
 using namespace std;
 
-float f_1 (float x)
-{
+float f_1(float x){
     return sin(x);
 }
 
-float f_2 (float x)
-{
+float f_2(float x){
     if (x >= 0){
-        return 2*floor(x/M_PI) - cos(fmod( x, M_PI )) + 1;
+        return 2*floor(x/M_PI) - cos(fmod(x, M_PI)) + 1;
     }
     else{
-        return 2*floor(x/M_PI) + cos(fmod( x, M_PI )) + 1;
+        return 2*floor(x/M_PI) + cos(fmod(x, M_PI)) + 1;
     }
 
 }
 
 typedef float (*Function) (float);
 
-float zweipunkt (Function fun, float x, float h)
-{
+float zweipunkt(Function fun, float x, float h){
     return (fun(x+h)-fun(x-h))/(2*h);
 }
 
-float vierpunkt (Function fun, float x, float h)
-{
+float vierpunkt(Function fun, float x, float h){
     return (-fun(x+2*h) +8*fun(x+h) -8*fun(x-h) +fun(x-2*h))/(12*h);
 }
 
 int main(){
     /* Definitions of variables */
-    float x_value;
-    x_value = M_PI;
+    float x_value = M_PI;
 
     /* Set output files */
     ofstream teil_a1, teil_a2, teil_b1, teil_b2;
@@ -50,12 +45,11 @@ int main(){
     teil_a1 << "\n" << h << "," << zweipunkt(f_1,x_value,h);
     }
 
-    float h_value;
-    h_value = pow(10,-2);
+    float h_value = pow(10, -2);
 
-    teil_a1 << "#h    y    delta_y";
+    teil_a2 << "#h    y    delta_y";
 
-    for (float x = -M_PI ; x <= M_PI; x = x + 0.01) {
+    for (float x = -M_PI; x <= M_PI; x += 0.01){
     teil_a2 << "\n" << x << "," << zweipunkt(f_1,x,h_value) << "," << abs(cos(x)-zweipunkt(f_1,x,h_value));
     }
 
