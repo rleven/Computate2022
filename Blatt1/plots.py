274435ae28bc7aac9ab8a847af338b81cb799864
David Gutnikov <david.gutnikov@tu-dortmund.de>
2022-04-14 02:30:35 +0200

    [Blatt1] Aufgabe 1b) fertig und Rechnung zu 1c) fertig

--- Blatt1/plots.py
+++ Blatt1/plots.py
@@ -8,12 +8,12 @@ h, f = np.genfromtxt("data/teil_a1.txt", comments = "#", delimiter = ',', unpack
 plt.plot(h, f, '-k', label="Approximation")
 plt.hlines(np.cos(np.pi), 10**(-3), 0.1, colors = 'blue', linestyles = 'dotted', label= 'Wahrer Wert')
 plt.xlabel("h")
-plt.ylabel(r"f'$_{\mathrm{1,Zweipunkt}}(0)$")
+plt.ylabel(r"f'$_{\mathrm{1,Zweipunkt}}(\pi)$")
 plt.xscale('log')
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("plots/aufgabe1_a1.pdf")
-plt.show()
+#plt.show()
 plt.close()
 
 x, y, del_y = np.genfromtxt("data/teil_a2.txt", comments = "#", delimiter = ',', unpack=True)
@@ -24,9 +24,62 @@ plt.ylabel(r"$\Delta$f'$_{\mathrm{1,Zweipunkt}}(x)$")
 plt.legend(loc="best")
 plt.tight_layout()
 plt.savefig("plots/aufgabe1_a2.pdf")
-plt.show()
+#plt.show()
 plt.close()
 
 
 ########################################################################################################################################################################
 ### Aufgabe 1b)
+h, f = np.genfromtxt("data/teil_b1.txt", comments = "#", delimiter = ',', unpack=True)
+
+plt.plot(h, f, '-k', label="Approximation")
+plt.hlines(-np.sin(np.pi/2), 10**(-3), 1, colors = 'blue', linestyles = 'dotted', label= 'Wahrer Wert')
+plt.xlabel("h")
+plt.ylabel(r"f''$_{\mathrm{1,Zweipunkt}}(\pi/2)$")
+plt.xscale('log')
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig("plots/aufgabe1_b1.pdf")
+#plt.show()
+plt.close()
+
+x, y, del_y = np.genfromtxt("data/teil_b2.txt", comments = "#", delimiter = ',', unpack=True)
+
+plt.plot(x, del_y, '-k', label="Fehler der Approximation")
+plt.xlabel(r"$x$")
+plt.ylabel(r"$\Delta$f''$_{\mathrm{1,Zweipunkt}}(x)$")
+plt.ticklabel_format(scilimits=(0,0))
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig("plots/aufgabe1_b2.pdf")
+#plt.show()
+plt.close()
+
+
+########################################################################################################################################################################
+### Aufgabe 1c)
+h, f = np.genfromtxt("data/teil_c1.txt", comments = "#", delimiter = ',', unpack=True)
+
+plt.plot(h, f, '-k', label="Approximation")
+plt.hlines(np.cos(np.pi), 10**(-5), 1, colors = 'blue', linestyles = 'dotted', label= 'Wahrer Wert')
+plt.xlabel("h")
+plt.ylabel(r"f'$_{\mathrm{1,Vierpunkt}}(\pi)$")
+plt.xscale('log')
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig("plots/aufgabe1_c1.pdf")
+plt.show()
+plt.close()
+
+x, y, del_y = np.genfromtxt("data/teil_c2.txt", comments = "#", delimiter = ',', unpack=True)
+
+plt.plot(x, del_y, '-k', label="Fehler der Approximation")
+plt.xlabel(r"$x$")
+plt.ylabel(r"$\Delta$f'$_{\mathrm{1,Vierpunkt}}(x)$")
+plt.ticklabel_format(scilimits=(0,0))
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig("plots/aufgabe1_c2.pdf")
+plt.show()
+plt.close()
+
