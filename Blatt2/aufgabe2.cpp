87e895e39f01344b9c908eb7bb8b23c7c00a9b0a
David Gutnikov <david.gutnikov@tu-dortmund.de>
2022-04-21 19:46:33 +0200

    [Blatt2] Aufgabe 2a) Euler hat nen Fehler.

--- Blatt2/aufgabe2.cpp
+++ Blatt2/aufgabe2.cpp
@@ -12,14 +12,14 @@ std::array<double,2> beschleunigung(std::array<double,2> a, int m, std::array<do
     double G = 6.67430 * pow(10, -11);
     a[0] = -G * m * 1/pow(pow(r_1[0] - r_2[0], 2) + pow(r_1[1] - r_2[1], 2), 1.5) * (r_1[0] - r_2[0]);
     a[1] = -G * m * 1/pow(pow(r_1[0] - r_2[0], 2) + pow(r_1[1] - r_2[1], 2), 1.5) * (r_1[1] - r_2[1]);
-
+    std::cout << a[0] << "  " << a[1] << "\n";
     return a;
 }
 
 void euler(double h, double T, int m_1, int m_2, std::array<double,2> r_1_start, std::array<double,2> r_2_start, std::array<double,2> v_1_start, std::array<double,2> v_2_start, std::array<double,2> a)
 {
-    std::ofstream teil_a1;
-    teil_a1.open("data/teil_a1.txt", std::ios_base::trunc);
+    std::ofstream teil_a_euler;
+    teil_a_euler.open("data/teil_a_euler.txt", std::ios_base::trunc);
 
     std::array<double,2> r_1_prev = r_1_start;
     std::array<double,2> v_1_prev = v_1_start;
@@ -39,19 +39,20 @@ void euler(double h, double T, int m_1, int m_2, std::array<double,2> r_1_start,
         beschl = beschleunigung(a, m_1, r_1_prev, r_2_prev);
 
         r_1_next[0] = r_1_prev[0] + (v_1_prev[0] * h);
-        v_1_next[0] = v_1_prev[0] + (beschl[0] / m_1 * h);
+        v_1_next[0] = v_1_prev[0] + (beschl[0]/m_1 * h);
         r_1_next[1] = r_1_prev[1] + (v_1_prev[1] * h);
-        v_1_next[1] = v_1_prev[1] + (beschl[1] / m_1 * h);
+        v_1_next[1] = v_1_prev[1] + (beschl[1]/m_1 * h);
 
         /*compute the values of mass 1 for the nth step*/
         beschl = beschleunigung(a, m_2, r_1_prev, r_2_prev);
 
         r_2_next[0] = r_2_prev[0] + (v_2_prev[0] * h);
-        v_2_next[0] = v_2_prev[0] + (beschl[0] / m_2 * h);
+        v_2_next[0] = v_2_prev[0] + (beschl[0]/m_2 * h);
         r_2_next[1] = r_2_prev[1] + (v_2_prev[1] * h);
-        v_2_next[1] = v_2_prev[1] + (beschl[1] / m_2 * h);
+        v_2_next[1] = v_2_prev[1] + (beschl[1]/m_2 * h);
 
-        teil_a1 << "\n" << r_1_next[0] << "," << r_1_next[1] << "," << r_2_next[0] << "," << r_2_next[1] << "," << v_1_next[0] << "," << v_1_next[1] << v_2_next[0] << "," << v_2_next[1];
+        /*write in file*/
+        teil_a_euler << "\n" << n << "  " << r_1_next[0] << "   " << v_1_next[0] << "   " << r_1_next[1] << "   " << v_1_next[1] << "   " << r_2_next[0] << "   " << v_2_next[0] << "   " << r_2_next[1] << "   " << v_2_next[1];
         
         /*overwrite values for next loop*/
         r_1_prev = r_1_next;
@@ -60,7 +61,7 @@ void euler(double h, double T, int m_1, int m_2, std::array<double,2> r_1_start,
         v_2_prev = v_2_next;
     }
 
-    teil_a1.close();
+    teil_a_euler.close();
 }
 
 int main()
@@ -78,11 +79,10 @@ int main()
     std::array<double,2> a;
 
     // std::array<double,2> ptr = beschleunigung(a, m_1, r_1_start, r_2_start);
-
     // std::cout << ptr[0] << "\n" << ptr[1];
 
     double T = 100;
-    double h = 10000;
+    double h = 0.1;
     euler(h, T, m_1, m_2, r_1_start, r_2_start, v_1_start, v_2_start, a);
 
     return 0;
--- Blatt2/plots.py
+++ Blatt2/plots.py
@@ -1,2 +1,17 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
+h, r1_x, v1_x, r1_y, v1_y, r2_x, v2_x, r2_y, v2_y =np.genfromtxt('data/teil_a_euler.txt' ,unpack=True)
+
+plt.figure()
+plt.plot(r1_x, r1_y,"r",linewidth=1,label=r'Trajektorie m$_1$')
+plt.plot(r2_x, r2_y,"b",linewidth=1,label=r'Trajektorie m$_2$')
+plt.plot(0, 1, "x",linewidth=1,label=r'Startpunkt m$_1$')
+plt.plot(0, -0.5, "x",linewidth=1,label=r'Startpunkt m$_2$')
+plt.xlabel("x")
+plt.ylabel(r'y')
+plt.grid()
+plt.legend(loc='best')
+# plt.show()
+plt.savefig("plots/flugweg.pdf")
+plt.clf()
