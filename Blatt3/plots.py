5719f05900a0c1c9e76e5bd7c1bd9356a13001c9
David Gutnikov <david.gutnikov@tu-dortmund.de>
2022-04-28 19:31:08 +0200

    [Blatt3] Cpp und Plots zu Aufgabe 1 fertig.

--- Blatt3/plots.py
+++ Blatt3/plots.py
@@ -1,3 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+
+
+# for alpha in np.arange(-1, 3):
+#     N, t, x, v = np.genfromtxt("data/aufgabe1_adams_" + str(alpha) + ".csv", unpack=True)
+#     plt.figure()
+#     plt.plot(t, x, linewidth=1.5, label=r"$\alpha=$" + str(alpha))
+#     plt.plot(t, v, linewidth=1.5, label=r"$\alpha=$" + str(alpha))
+#     plt.xlabel(r"$t$")
+#     plt.ylabel(r'$x$')
+#     plt.grid()
+#     plt.legend(loc='best')
+#     plt.show()
+#     plt.savefig("plots/aufgabe1_a_" + str(alpha) + ".pdf")
+#     plt.clf()
+    
+N, t, x, v = np.genfromtxt("data/aufgabe1_adams_energieerhaltung.csv", unpack=True)
+plt.figure()
+plt.plot(t, -x**2 + v**2, linewidth=1.5, label=r"$\alpha=0.1$")
+plt.xlabel(r"$t$")
+plt.ylabel(r'$E$')
+plt.grid()
+plt.legend(loc='best')
+plt.show()
+plt.savefig("plots/aufgabe1_a_" + str(alpha) + ".pdf")
+plt.clf()
