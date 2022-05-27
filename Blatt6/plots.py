9822a22f33ac75230bcd1cf2042b127a76ec3d90
David Gutnikov <david.gutnikov@tu-dortmund.de>
2022-05-27 13:41:18 +0200

    [Blatt6] Aufgabe 1 ist komplett fertig.

--- Blatt6/plots.py
+++ Blatt6/plots.py
@@ -14,7 +14,7 @@ fig, ax = plt.subplots()
 ax.plot(r, x_fixpoints, 'k.', markersize=0.05)
 
 ax.set_xlabel(r"r")
-ax.set_ylabel(r"x")
+ax.set_ylabel(r"x_n")
 
 plt.savefig("plots/fixpoints_logistic.png")
 
@@ -31,45 +31,9 @@ fig, ax = plt.subplots()
 ax.plot(r, x_fixpoints, 'k.', markersize=0.05)
 
 ax.set_xlabel(r"r")
-ax.set_ylabel(r"x")
+ax.set_ylabel(r"x_n")
 
 plt.savefig("plots/fixpoints_cubic.png")
 
 plt.show()
 plt.close()
-
-
-
-# ### Aufgabe 1 ###
-# aufgabe = ['a', 'b', 'c', 'd']
-# cmap_aufgabe = ['Reds' , 'Reds', 'Reds', 'RdBu_r']
-# L = 21
-
-# for temp_aufgabe, temp_cmap in zip(aufgabe, cmap_aufgabe):
-#     phi = np.transpose(np.genfromtxt("data/Potential_" + temp_aufgabe + ".csv", unpack=True))
-#     N_steps = int(phi.shape[0] / L)
-
-#     # Die Potentialwerte in ein angemessenes Format, hier 3d-Array, (für die Animation) bringen
-#     phi_3d = np.empty(shape=(0, L, L))
-#     for i in np.arange(N_steps):
-#         temp = phi[L*i:L*i+L, :]
-#         phi_3d = np.append(phi_3d, np.transpose(np.atleast_3d(temp)), axis=0)
-
-
-#     fig, ax = plt.subplots(figsize=(6, 6))
-#     quad = ax.imshow(phi_3d[N_steps-1], cmap=temp_cmap)
-
-#     cb = fig.colorbar(quad)
-#     cb.formatter.set_powerlimits((0, 0))
-#     cb.minorticks_on()
-
-#     ax.set_xlabel(r"Index der Gitterpunkte in x-Richtung")
-#     ax.set_ylabel(r"Index der Gitterpunkte in y-Richtung")
-
-#     plt.savefig("plots/Potential_" + temp_aufgabe + ".png")
-
-#     # plt.show()
-#     plt.close()
-
-
-
