9a126cf24bd2fca6272cff8fd5c7754aae13afdc
David Gutnikov <david.gutnikov@tu-dortmund.de>
2022-06-10 13:55:03 +0200

    [Blatt8] Aufgabe 1 fertig

--- Blatt8/plots.py
+++ Blatt8/plots.py
@@ -11,24 +11,24 @@ fig, axs = plt.subplots(3, 1, sharex=True, figsize=(5, 3*3))
 text = [r"Matrixerzeugung", r"QR-Zerlegung", r"Lösung"]
 
 for i, word in enumerate(text):
-    axs[i].plot(N, t[i,:], ".", markersize=2)
+    axs[i].plot(N, t[i+1,:], ".", markersize=2)
     axs[i].set_xscale("log")
     axs[i].set_yscale("log")
     axs[i].set_ylabel(word)
 
 fig.supylabel(r"Laufzeiten [s]")
-axs[2].set_xlabel(r"N")
+axs[2].set_xlabel(r"$N$")
 
 plt.tight_layout()
 plt.savefig("plots/Laufzeiten.png")
 
-# plt.show()
+plt.show()
 plt.close()
 
 
 ### Aufgabe 1c): Plotten und extrapolieren der Gesamtlaufzeit
-def fit_function(x, a, b, c, d, e, f):
-    return a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
+def fit_function(x, a, b, c, d):
+    return a*x**3 + b*x**2 + c*x + d
 
 t_gesamt = t[1,:] + t[2,:] + t[3,:]
 
@@ -36,6 +36,7 @@ fig, ax = plt.subplots()
 
 ax.plot(N, t_gesamt, ".", markersize=2)
 ax.set_ylabel(r"Gesamtlaufzeit [s]")
+ax.set_xlabel(r"$N$")
 
 params, cov = curve_fit(fit_function, N, t_gesamt)
 N_fit = np.linspace(0, 10**6, num=10**6)
@@ -46,5 +47,5 @@ ax.set_yscale("log")
 plt.tight_layout()
 plt.savefig("plots/Gesamtlaufzeit.png")
 
-plt.show()
+# plt.show()
 plt.close()
